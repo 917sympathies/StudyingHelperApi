d473932cc2097192a34025f998d2f00ded08c074
Marat <maraturazbakhtin@yandex.ru>
2023-10-18 09:58:14 +0400

    Созданы основные модели, инициализирована база данных

---
+++ StudyingHelperApi/Migrations/20231018055436_Initial.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudyingHelperApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                    table.UniqueConstraint("AK_users_Username", x => x.Username);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "workspaces",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_workspaces", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_workspaces_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WorkspaceId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tasks_workspaces_WorkspaceId",
+                        column: x => x.WorkspaceId,
+                        principalTable: "workspaces",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tasks_WorkspaceId",
+                table: "tasks",
+                column: "WorkspaceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_workspaces_UserId",
+                table: "workspaces",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tasks");
+
+            migrationBuilder.DropTable(
+                name: "workspaces");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
---
+++ StudyingHelperApi/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿namespace StudyingHelperApi.Models
+{
+    public enum TaskState
+    {
+        Acutal,
+        Done
+    }
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+    }
+}
